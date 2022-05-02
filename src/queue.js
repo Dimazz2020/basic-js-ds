0f31fba6ed044578bcc85e64cc5790e1aa672982
Dimazz2020 <88279178+Dimazz2020@users.noreply.github.com>
2022-05-03 01:27:57 +0300

    Update queue.js

--- src/queue.js
+++ src/queue.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {NotImplementedError} = require('../extensions/index.js');
 
 // const { ListNode } = require('../extensions/list-node.js');
 
@@ -13,36 +13,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+class ListNode {
+    constructor(x) {
+        this.value = x;
+        this.next = null;
+    }
+}
+
 class Queue {
-	constructor () {
+    constructor() {
+        this.queue;
+        this.last;
+    }
 
-		this.head = null;
+    getUnderlyingList() {
+        return this.queue;
+    }
 
-	}
+    enqueue(value) {
+        if (!this.queue) {
+            this.queue = new ListNode(value);
+            this.last = this.queue;
+        } else {
+            this.last.next = new ListNode(value);
+            this.last = this.last.next;
+        }
+    }
 
-	getUnderlyingList() {
-		return this.head;
-	}
-
-	enqueue(data) {
-		let current = this.head;
-		if (this.head === null) {
-			this.head = new ListNode(data);
-		} else {
-			while (current.next) {
-				current = current.next;
-			}
-			current.next = new ListNode(data);
-		}
-	}
-
-	dequeue() {
-		let current = this.head;
-		this.head = this.head.next;
-		return current.value;
-	}
+    dequeue() {
+        let el = this.queue.value;
+        this.queue = this.queue.next;
+        return el;
+    }
 }
 
 module.exports = {
-	Queue
+    Queue
 };
